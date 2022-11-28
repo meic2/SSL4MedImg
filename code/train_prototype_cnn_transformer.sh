7aaab4c23d9ae4ae455c4da236b80f25f8a9f505
Luoyao Chen <lc4866@log-3.hpc.nyu.edu>
2022-11-28 13:07:17 -0500

    new commit

--- code/train_prototype_cnn_transformer.sh
+++ code/train_prototype_cnn_transformer.sh
@@ -1,5 +1,5 @@
 python train_prototype_cnn_transformer.py \
 --root_path '../../dataset/Dermatomyositis' \
---exp 'Dermatomyositis_prototype/pre_train' \
+--exp 'Dermatomyositis_prototype/test' \
 --data_class 2 \
 --batch_size 16 
